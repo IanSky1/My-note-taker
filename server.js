a6aaad9019e5ea1ce0bcaa4d4145933976f96961
Ian <Ian.Mohn1@gmail.com>
2022-05-17 07:43:21 -0500

    Updated db.json file

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs')
 
 const { notes } = require('./db/db.json')
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 4001
 const app = express()
 
 app.use(express.urlencoded({ extended: true }))
